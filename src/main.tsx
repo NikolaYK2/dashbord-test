f48a6edd89b12d0808c60ecc5fd5dd51bbe7d05f
k2 <izmail.k2@gmail.com>
2025-02-26 21:36:32 +0300

    chore: add import fonts & styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/700.css";
+import "@fontsource/nunito/700.css";
+import "@/assets/styles/index.scss";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
