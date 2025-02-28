08a42f6b0e1e9ec03de9487e6dedca7b8163fb5f
k2 <izmail.k2@gmail.com>
2025-03-01 00:47:23 +0300

    feat: add props - change type

--- src/common/components/find/Find.tsx
+++ src/common/components/find/Find.tsx
@@ -1,11 +1,33 @@
 import s from "./Find.module.scss";
 import { Icon } from "@/common/components/icons/Icon.tsx";
+import { Typography } from "@/common/components/typography";
+import { ChangeEvent } from "react";
 
-export const Find = () => {
+type FindProps = {
+  quantityTest: number;
+  searchTerm: string;
+  setSearchTerm: (value: string) => void;
+  className?: string;
+};
+export const Find = ({ quantityTest = 0, searchTerm, setSearchTerm, className='' }: FindProps) => {
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.currentTarget.value);
+  };
+
   return (
-    <section className={s.containerFind}>
-      <input className={s.input} type="search" placeholder={"What test are you looking for?"} />
+    <section className={`${s.containerFind} ${className}`}>
+      <input
+        className={s.input}
+        type="text"
+        placeholder={"What test are you looking for?"}
+        value={searchTerm}
+        onChange={onChange}
+      />
       <Icon className={s.icon} iconId={"Find"} width={14} height={14} />
+      <Typography className={s.tests} variant={"robReg14"}>
+        {quantityTest}
+        <span>tests</span>
+      </Typography>
     </section>
   );
 };
