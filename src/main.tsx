f95a52b15ad597ac645fa6c9c4468a674d5254ad
k2 <izmail.k2@gmail.com>
2025-02-28 16:19:32 +0300

    refactor: remove global style

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,11 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+
+import App from "@app/routes/_index.tsx";
 
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/montserrat/600.css";
 import "@fontsource/montserrat/700.css";
 import "@fontsource/nunito/700.css";
-import "@/assets/styles/index.scss";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+createRoot(document.getElementById("root")!).render(<App />);
