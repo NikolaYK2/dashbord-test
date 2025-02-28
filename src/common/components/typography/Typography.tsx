554002a759184742afcfb8f8222b88a1c0927ff3
k2 <izmail.k2@gmail.com>
2025-02-28 16:14:36 +0300

    refactor: change export

--- src/common/components/typography/Typography.tsx
+++ src/common/components/typography/Typography.tsx
@@ -1,5 +1,5 @@
 import { ComponentPropsWithoutRef, ElementType } from "react";
-import s from "src/common/components/typography/Typography.module.scss";
+import s from "./Typography.module.scss";
 
 type VariantType =
   | "h1"
@@ -22,7 +22,7 @@ export const Typography = <T extends ElementType = "p">(props: TypographyProps<T
   const { variant = "p", className, ...restProps } = props;
   const Component: ElementType = variant && getComponent(variant as VariantType);
 
-  return <Component className={`${s[variant]} ${className}`} {...restProps} />;
+  return <Component className={`${s.default} ${s[variant]} ${className}`} {...restProps} />;
 };
 
 function getComponent(variant: VariantType) {
